6551b2e5f9fc06b8c35a8ed73d79d6b27149a36f
bathien <bathien2601@gmail.com>
2017-05-08 13:55:49 +0700

    finish user follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
     foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  has_many :activities, dependent: :destroy
   has_many :lessons, dependent: :destroy
 
   before_save :downcase_email
@@ -75,6 +74,18 @@ class User < ApplicationRecord
 
   def forget
     update_attributes remember_digest: nil
+  end
+
+  def follow other_user
+    active_relationships.create followed_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
   private
