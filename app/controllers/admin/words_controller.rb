f59eaa54d65651b2d0a2f3fbb0e417fe78dd5d54
bathien <bathien2601@gmail.com>
2017-05-11 06:48:53 +0700

    admin show all word

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,6 +1,15 @@
 class Admin::WordsController < ApplicationController
   before_action :logged_in_user, :verify_admin
   before_action :load_word, only: :show
+  before_action :load_all_category, only: :index
+
+  def index
+    params[:search_word] ||= ""
+    @words = Word.select(:id, :content, :category_id).includes(:category)
+      .filter_category(params[:category_id])
+      .get_all(params[:search_word])
+      .page(params[:page]).per Settings.word.per_page
+  end
 
   def show
     @answers = @word.answers
